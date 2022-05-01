83cb355f4fce68704102692606dc2e4240aaec5e
Sai Kumar <mutoopuris@gmail.com>
2022-05-01 15:23:52 -0400

    variables.

---
+++ app.js
@@ -0,0 +1,28 @@
+let i = 10;
+
+if (i > 0) {
+  console.log(i);
+}
+
+if (i) {
+  console.log(i);
+}
+
+const myArray = [4, true, "abc", null, 5, 10];
+if (myArray.length > 5) {
+  console.log("Array is large");
+} else {
+  console.log("Array is small");
+}
+
+let color = "yasdf";
+
+if (color === "green") {
+  console.log("It is green color");
+} else if (color === "yellow") {
+  console.log("It is yellow color");
+} else if (color === "red") {
+  console.log("It is red color");
+} else {
+  console.log("Color is unknown");
+}
